8c5a2dfc95430c7cd78f26671b98affbf2de8f2d
manhhomienbienthuy <1755796+manhhomienbienthuy@users.noreply.github.com>
2025-07-30 16:45:12 +0900

    Update 452b.py

--- 452b.py
+++ 452b.py
@@ -1,14 +1,40 @@
-#!/usr/bin/env python3
+def dist(x, y):
+    return (x[0] - y[0]) * (x[0] - y[0]) + (x[1] - y[1]) * (x[1] - y[1])
 
 n, m = map(int, input().split())
-a = ([[0, 1], [0, m], [0, 0], [0, m - 1]],
-     [[1, 0], [n, 0], [0, 0], [n - 1, 0]],
-     [[0, 1], [n, m], [0, 0], [n, m - 1]],
-     [[1, 0], [n, m], [0, 0], [n - 1, m]],
-     [[0, 0], [n, m], [0, m], [n, 0]],
-     [[0, 0], [n, m], [n, 0], [0, m]]
-     )
+result = set((
+    (n, m),
+    (n, m-1),
+    (n-1, m-1),
+    (n-1, m),
+    (n, 1),
+    (n, 0),
+    (n-1, 0),
+    (n-1, 1),
+    (1, 1),
+    (1, 0),
+    (0, 0),
+    (0, 1),
+    (1, m),
+    (1, m-1),
+    (0, m-1),
+    (0, m),
+))
+result = list(filter(lambda x: n >= x[0] >= 0 and m >= x[1] >= 0, result))
 
+tmp = -1
+for i in result:
+    for j in result:
+        for k in result:
+            for l in result:
+                if i == j or i == k or i == l or j == k or j == l or k == l:
+                    continue
+                t = dist(i, j) + dist(j, k) + dist(k, l)
+                if t > tmp:
+                    tmp = t
+                    a1, a2, a3, a4 = i, j, k, l
 
-for x in max(a, key=lambda a: (len(set(map(tuple, a))) == len(a)) * all(map((lambda x: min(x[1], x[0]) >= 0), a)) * sum(((a[i][0] - a[i + 1][0]) ** 2 + (a[i][1] - a[i + 1][1]) ** 2) ** 0.5 for i in range(len(a) - 1))):
-    print(*x)
+print(*a1)
+print(*a2)
+print(*a3)
+print(*a4)
