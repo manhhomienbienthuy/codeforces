215a9e3e0338902cf8f99263bc384dc96085b685
manhhomienbienthuy <1755796+manhhomienbienthuy@users.noreply.github.com>
2025-08-15 09:11:01 +0900

    Update 2131a.py

--- 2131a.py
+++ 2131a.py
@@ -3,5 +3,5 @@ for _ in range(t):
     n = int(input())
     a = list(map(int, input().split()))
     b = list(map(int, input().split()))
-    inc = sum(a[i] - b[i] if a[i] >= b[i] else 0 for i in range(n))
+    inc = sum(max(a[i] - b[i], 0) for i in range(n))
     print(inc + 1)
