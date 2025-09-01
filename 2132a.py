23ebf52093de2dcc3aae4bfa3ff59a9d8b075b6a
manhhomienbienthuy <1755796+manhhomienbienthuy@users.noreply.github.com>
2025-09-01 15:02:48 +0900

    Create 2132a.py

---
+++ 2132a.py
@@ -0,0 +1,13 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = input()
+    m = int(input())
+    b = input()
+    c = input()
+    for i in range(m):
+        if c[i] == 'D':
+            a = a + b[i]
+        else:
+            a = b[i] + a
+    print(a)
