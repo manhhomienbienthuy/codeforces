c368716a71ecde270f93819c205c43084c209eb8
manhhomienbienthuy <1755796+manhhomienbienthuy@users.noreply.github.com>
2026-04-13 22:54:50 +0900

    Refactor calculations for efficiency and clarity

--- 2215a.py
+++ 2215a.py
@@ -2,28 +2,15 @@ t = int(input())
 for _ in range(t):
     n, k, p, q = map(int, input().split())
     a = list(map(int, input().split()))
-    calc_p = [-1] * n
-    calc_q = [-1] * n
-    calc_min = [-1] * n
-    ssmin, smin, sp, sq = 0, 0, 0, 0
-    for i in range(n):
-        calc_p[i] = a[i] % p % q
-        calc_q[i] = a[i] % q % p
-        calc_min[i] = min(calc_p[i], calc_q[i])
-        ssmin += calc_min[i]
-        if i < k:
-            smin += calc_min[i]
-            sp += calc_p[i]
-            sq += calc_q[i]
-    window_diff = sp - smin
-    if sq - smin < window_diff:
-        window_diff = sq - smin
+    calc_p = [x % p % q for x in a]
+    calc_q = [x % q % p for x in a]
+    calc_min = [min(x, y) for (x, y) in zip(calc_p, calc_q)]
+    ssmin = sum(calc_min)
+    smin, sp, sq = sum(calc_min[:k]), sum(calc_p[:k]), sum(calc_q[:k])
+    window_diff = min(sp - smin, sq - smin)
     for i in range(1, n - k + 1):
         smin = smin - calc_min[i - 1] + calc_min[i + k - 1]
         sq = sq - calc_q[i - 1] + calc_q[i + k - 1]
         sp = sp - calc_p[i - 1] + calc_p[i + k - 1]
-        if sp - smin < window_diff:
-            window_diff = sp - smin
-        if sq - smin < window_diff:
-            window_diff = sq - smin
+        window_diff = min(window_diff, sp - smin, sq - smin)
     print(ssmin + window_diff)
