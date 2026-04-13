1bf1465f0360587248d4eb7b70b18a761f652b04
manhhomienbienthuy <1755796+manhhomienbienthuy@users.noreply.github.com>
2026-04-13 22:56:12 +0900

    Fix sum calculation in output for 2215a.py

--- 2215a.py
+++ 2215a.py
@@ -5,7 +5,6 @@ for _ in range(t):
     calc_p = [x % p % q for x in a]
     calc_q = [x % q % p for x in a]
     calc_min = [min(x, y) for (x, y) in zip(calc_p, calc_q)]
-    ssmin = sum(calc_min)
     smin, sp, sq = sum(calc_min[:k]), sum(calc_p[:k]), sum(calc_q[:k])
     window_diff = min(sp - smin, sq - smin)
     for i in range(1, n - k + 1):
@@ -13,4 +12,4 @@ for _ in range(t):
         sq = sq - calc_q[i - 1] + calc_q[i + k - 1]
         sp = sp - calc_p[i - 1] + calc_p[i + k - 1]
         window_diff = min(window_diff, sp - smin, sq - smin)
-    print(ssmin + window_diff)
+    print(sum(calc_min) + window_diff)
