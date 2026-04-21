e505fb0c5af6878efac1bd7d4c8ec601ada45a47
manhhomienbienthuy <1755796+manhhomienbienthuy@users.noreply.github.com>
2026-04-21 17:33:11 +0900

    Create 2220c.py

---
+++ 2220c.py
@@ -0,0 +1,14 @@
+from math import isqrt
+
+t = int(input())
+for _ in range(t):
+    p, q = map(int, input().split())
+    t = p + 2 * q
+    for n in range(1, isqrt(p / 2 + q) + 1):
+        if not (t - n) % (2 * n + 1):
+            m = (t - n) // (2 * n + 1)
+            if p >= m - n:
+                print(n, m)
+                break
+    else:
+        print(-1)
