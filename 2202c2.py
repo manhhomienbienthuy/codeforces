3c05a1ca5e8245cffc09f6396c8b9b4c61fd5182
manhhomienbienthuy <1755796+manhhomienbienthuy@users.noreply.github.com>
2026-05-06 11:52:55 +0900

    Optimize answer calculation in 2202c2.py
    Refactor the algorithm to calculate the answer using a stack without intermediate sequences.

--- 2202c2.py
+++ 2202c2.py
@@ -2,30 +2,15 @@ t = int(input())
 for _ in range(t):
     n = int(input())
     a = list(map(int, input().split()))
-    seq = []
     stack = []
-    for i in range(n):
-        cur = []
-        while stack and a[stack[-1]] != a[i] - 1:
-            cur.append(stack.pop())
-        if cur:
-            seq.append(stack + cur[::-1])
-        stack.append(i)
-    seq.append(stack)
     ans = 0
-    cur = [-1]
-    for s in seq:
-        if s[0] != cur[0]:
-            l, r = s[0], n - 1 - s[-1]
-            ans += (n * (n + 1) - l * (l + 1) - r * (r + 1)) // 2
-            cur = s
+    for i in range(n):
+        while stack and a[stack[-1]] != a[i] - 1:
+            stack.pop()
+        if stack:
+            prev = stack[-1]
         else:
-            cur_n = n
-            l, r = s[-1], n - 1 - s[-1]
-            for j in range(len(s) - 1, 0, -1):
-                if s[j] != s[j - 1] + 1:
-                    l = s[j] - s[j - 1] - 1
-                    cur_n = n - s[j - 1] - 1
-                    break
-            ans += (cur_n * (cur_n + 1) - l * (l + 1) - r * (r + 1)) // 2
+            prev = -1
+        ans += (i - prev) * (n - i)
+        stack.append(i)
     print(ans)
