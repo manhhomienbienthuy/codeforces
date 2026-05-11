4a00994abd0b2dcd82c0b3b83c4e482f2ed0c723
manhhomienbienthuy <1755796+manhhomienbienthuy@users.noreply.github.com>
2026-05-11 09:56:13 +0900

    update 2178c.py

--- 2178c.py
+++ 2178c.py
@@ -2,14 +2,14 @@ t = int(input())
 for _ in range(t):
     n = int(input())
     a = list(map(int, input().split()))
-    pref = [0] * (n + 1)
-    pref[1] = a[0]
-    for i in range(2, n + 1):
-        pref[i] = pref[i - 1] + abs(a[i - 1])
-    suf = [0] * (n + 1)
-    for i in range(n - 1, -1, -1):
-        suf[i] = suf[i + 1] + a[i]
-    ans = -10**18
-    for i in range(n):
-        ans = max(ans, pref[i] - suf[i + 1])
+    suf = sum(a) - a[0]
+    ans = -suf
+    pref = a[0]
+    for i in range(1, n):
+        suf -= a[i]
+        ans = max(ans, pref - suf)
+        if i == 0:
+            pref = a[0]
+        else:
+            pref += abs(a[i])
     print(ans)
