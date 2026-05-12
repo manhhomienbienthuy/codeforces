d5721b568e6cb31cf01f31a839c5f58d5c74fe7d
manhhomienbienthuy <1755796+manhhomienbienthuy@users.noreply.github.com>
2026-05-12 14:17:18 +0900

    Refactor 2205c.py

--- 2205c.py
+++ 2205c.py
@@ -1,17 +1,19 @@
 t = int(input())
-
 for _ in range(t):
     n = int(input())
-    Q = []
-    blogs = []
+    b = []
     for i in range(n):
-        a = list(map(int, input().split()))[1:]
-        a = list(dict.fromkeys(a).keys())
-        blogs.append(a[::-1])
-    while blogs:
-        b = min(blogs)
-        Q += b
-        for i in range(len(blogs)):
-            blogs[i] = [k for k in blogs[i] if k not in Q]
-        blogs = [k for k in blogs if k]
-    print(*Q)
+        x = list(map(int, input().split()))
+        s = set()
+        b.append([i for i in x[:0:-1] if not (i in s or s.add(i))])
+    ans = []
+    while b:
+        ans += min(b)
+        r = []
+        for i in range(len(b) - 1, -1, -1):
+            b[i] = [j for j in b[i] if j not in ans]
+            if not b[i]:
+                r.append(i)
+        for i in r:
+            del b[i]
+    print(*ans)
