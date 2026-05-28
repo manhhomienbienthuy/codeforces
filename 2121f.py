b19a4e4a157090082ba61ba478c5a8142dff7aa7
manhhomienbienthuy <1755796+manhhomienbienthuy@users.noreply.github.com>
2026-05-28 15:20:22 +0900

    update 2121f.py

--- 2121f.py
+++ 2121f.py
@@ -2,21 +2,21 @@ t = int(input())
 for _ in range(t):
     n, s, x = map(int, input().split())
     a = list(map(int, input().split()))
+    ans = 0
+    before, after = {}, [0]
+    pref = 0
+    for v in a:
+        if v > x:
+            before.clear()
+            after = [0]
+            pref = 0
+            continue
+        if v == x:
+            for k in after:
+                before[k] = before.get(k, 0) + 1
+            after = []
+        pref += v
+        after.append(pref)
+        ans += before.get(pref - s, 0)
 
-    def calc(k):
-        res = 0
-        pref = 0
-        cnt = {0: 1}
-
-        for v in a:
-            if v > k:
-                pref = 0
-                cnt = {0: 1}
-                continue
-            pref += v
-            res += cnt.get(pref - s, 0)
-            cnt[pref] = cnt.get(pref, 0) + 1
-
-        return res
-
-    print(calc(x) - calc(x - 1))
+    print(ans)
