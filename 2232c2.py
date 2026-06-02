d226cda73eb2d6e583e63e0f8acb85b8ad511538
manhhomienbienthuy <1755796+manhhomienbienthuy@users.noreply.github.com>
2026-06-02 12:55:09 +0900

    Refactor seating calculation logic in 2232c2.py

--- 2232c2.py
+++ 2232c2.py
@@ -3,35 +3,32 @@ for _ in range(t):
     n, x, s = map(int, input().split())
     u = input()
 
-    def calc(k):
-        table = 0
-        seated = 0
-        ai = 0
-
-        for c in u:
-            if seated == x * s:
-                break
-            if c == 'I' or (c == 'A' and ai < k):
-                if table < x:
-                    table += 1
-                    seated += 1
-                    ai += c == 'A'
-                continue
-            if seated < table * s:
-                seated += 1
-
-        return seated
-
-    l, r = 0, x
-    while r - l > 3:
-        m1 = (2 * l + r) // 3
-        m2 = (l + 2 * r) // 3
-        if calc(m1) < calc(m2):
-            l = m1
-        else:
-            r = m2
-
     ans = 0
-    for k in range(l, r + 1):
-        ans = max(ans, calc(k))
+    table = 0
+    cnt_ae = 0
+    for c in u:
+        if ans == x * s:
+            break
+
+        if c == 'I':
+            if table < x:
+                table += 1
+                ans += 1
+            continue
+        if c == 'E':
+            if ans < table * s:
+                ans += 1
+            else:
+                if cnt_ae > 0 and table < x:
+                    cnt_ae -= 1
+                    table += 1
+                    ans += 1
+            continue
+        if ans < table * s:
+            ans += 1
+            cnt_ae += 1
+        elif table < x:
+            table += 1
+            ans += 1
+
     print(ans)
