e0e9335ddd681b3f6da3a8d72e336cdee726137e
manhhomienbienthuy <1755796+manhhomienbienthuy@users.noreply.github.com>
2026-06-09 15:23:00 +0900

    2234c.cpp

---
+++ 2234c.py
@@ -0,0 +1,16 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    h = list(map(int, input().split()))
+
+    for l in range(n):
+        w = [0] * n
+        for i in range(l + 1, l + n):
+            w[i % n] = max(w[(i - 1) % n], h[(i - 1) % n])
+        for i in range(l + n - 1, l, -1):
+            if max(w[i % n], w[(i + 1) % n]) > h[i % n] and w[i % n] != w[(i + 1) % n]:
+                w[i % n] = max(w[(i + 1) % n], h[i % n])
+            else:
+                break
+        print(sum(w), end=' ')
+    print()
