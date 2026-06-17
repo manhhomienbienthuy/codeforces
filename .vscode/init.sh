fc9ba287fa47c97f27405c464d8c1292f5fbde7b
manhhomienbienthuy <1755796+manhhomienbienthuy@users.noreply.github.com>
2026-06-17 15:41:12 +0900

    Refactor main function declaration in init.sh

--- .vscode/init.sh
+++ .vscode/init.sh
@@ -15,8 +15,7 @@ cr() {
     echo "#include <bits/stdc++.h>" >> $round$1.cpp
     echo "using namespace std;"  >> $round$1.cpp
     echo ""  >> $round$1.cpp
-    echo "int main()"  >> $round$1.cpp
-    echo "{"  >> $round$1.cpp
+    echo "int main() {"  >> $round$1.cpp
     echo "    ios::sync_with_stdio(false);"  >> $round$1.cpp
     echo "    cin.tie(nullptr);"  >> $round$1.cpp
     echo ""  >> $round$1.cpp
